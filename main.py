190045cc25ccfa2100fe5e8f53eb84e195769a9c
pokepup <s213012@dtu.dk>
2023-02-04 23:52:48 +0100

    Optimized function: similarity_matrix Created PCoA function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from Bio.Blast.Applications import NcbiblastnCommandline, NcbiblastpCommandline
 #from Bio import SeqIO, Align, SeqRecord
+import matplotlib.pyplot as plt
+from sklearn.manifold import MDS
 from argparse import ArgumentParser
 import numpy as np
 import pandas as pd
@@ -26,7 +28,7 @@ outdir = args.outdir
 ### class virome ###
 #------------------#
 
-class virome():
+class tools():
     def __init__(self, infile, outdir):
         self.infile = infile
         self.filename = '.'.join(os.path.split(infile)[1].split('.')[:-1])
@@ -35,27 +37,44 @@ class virome():
         if not os.path.exists(self.outdir):
             os.mkdir(self.outdir)
             print(f"Directory path made: {self.outdir}")
-
-    def pairwise(self):
-        cline = NcbiblastnCommandline(query = infile, subject=infile, outfmt=6, out=self.blastfile, max_hsps=1)
-        return cline()
-
+        
     def similarity_matrix(self):
-        # extracting OTU labels and perc_identity scores
-        matrix = np.loadtxt(self.blastfile, dtype=str, delimiter='\t', usecols=(0,1,2))
-        matrix[:,2] = matrix[:,2].astype(float)*0.01
-        print(matrix[:,2])
-        df = pd.DataFrame(matrix, columns=['ID1' , 'ID2', 'score'])
-        # pivot & index table
-        pivot = pd.pivot_table(df,index='ID1',columns='ID2',values='score', aggfunc=lambda x: ' '.join(x))
-        pivot_index = pivot.index.union(pivot.columns)
-        self.table = pivot.reindex(index=pivot_index, columns=pivot_index, fill_value=0.0)
-        self.matrix = self.table.to_numpy(na_value=0.0)
-        print(self.matrix)
+        # load and pivot table
+        df = pd.read_csv(self.blastfile, sep='\t')
+        df = df[df.columns[0:3]]
+        df[df.columns[2]] = df[df.columns[2]].apply(lambda x: x*0.01)
+        pivot = pd.pivot_table(df, index=df.columns[0], columns=df.columns[1], values=df.columns[2], fill_value=0.0)
+        # saving object variables
+        self.table = pivot
+        self.matrix = self.table.to_numpy()
 
     def save_similarity_matrix(self):
         #os.remove(self.blastfile)
         return self.table.to_csv(os.path.join(self.outdir,self.filename + ".csv"), index=False, sep='\t')
+
+    def PCOA(self):
+        # Transforms data
+        mds = MDS(random_state=0, metric=True)
+        test = mds.fit_transform(self.table)
+
+        # visualization
+        plt.scatter(test[:,0], test[:,1])
+        plt.xlabel('Coordinate 1')
+        plt.ylabel('Coordinate 2')
+        # TO DO: proper legenda with colors, do grouping of data into one label ?
+        """
+        for i, txt in enumerate(self.table.index):
+            plt.annotate(txt, (test[:,0][i]+.3, test[:,1][i]))
+        """
+        return plt.show()
+
+class virome(tools):
+    def __init__(self, infile, outdir):
+        super().__init__(infile, outdir)
+
+    def pairwise(self):
+        cline = NcbiblastnCommandline(query = infile, subject=infile, outfmt=6, out=self.blastfile, max_hsps=1)
+        return cline()
 
 class proteomics(virome):
     def __init__(self, infile, outdir):
@@ -73,26 +92,7 @@ start_time = time.time()
 test = proteomics(infile, outdir)
 #test.pairwise()
 test.similarity_matrix()
-test.save_similarity_matrix()
+test.PCOA()
+
+#test.save_similarity_matrix()
 print(f"Elapsed time: {(time.time()-start_time)} seconds")
-
-
-
-# list of indexed names
-#feature_index = np.asarray((np.unique(x, return_counts=True))).T
-
-# size
-#SIZE = len(feature_index)
-#
-## list of names
-#feature_names = [feature_index[i][0] for i in range(SIZE)]
-#
-## empty score matrix:
-#css_matrix = np.zeros(shape=[SIZE, SIZE], dtype=np.float32)
-
-#for m in range(SIZE):
-#    for n in range(int(feature_index[m][1])):
-#        if ID[n] in feature_names:
-#            css_matrix[m][feature_names.index(ID[n])] = score[feature_names.index(ID[n])]
-#
-#np.savetxt('OTU_scores.txt', css_matrix, fmt='%.2f')
