d262eaf5e15349521eb4294080738bd50713a1cc
pokepup <s213012@dtu.dk>
2023-03-16 17:12:04 +0100

    Eigen optimization

---
+++ gradient_descent.py
@@ -0,0 +1,470 @@
+import scipy.sparse as sparse
+import scipy
+import numpy as np
+import pandas as pd
+import random, time, math, pickle
+import matplotlib.pyplot as plt
+import itertools, os, mkl
+import skbio
+import seaborn as sns
+import multiprocessing as mp
+from sklearn.metrics.pairwise import cosine_similarity
+
+#---------------------------------------------------------------------------------------------------------------------#
+# Functions under development
+#---------------------------------------------------------------------------------------------------------------------#
+
+def PCOA(sparse_matrix):
+    # Compute principial coordinated from sparse matrix
+    #n = sparse_matrix.shape[0]
+    #centered_matrix = np.eye(n) - np.ones((n, n))/n
+    #X = -(1/n) * centered_matrix @ sparse_matrix @ centered_matrix
+    #eigvals, eigvecs = sparse.linalg.eigs(X, k=n)
+    #coordinates = eigvecs @ np.diag(np.sqrt(eigvals))
+    symmetric = sparse.csr_matrix.dot(sparse_matrix, sparse_matrix.T) / 2
+    symmetric.setdiag(1)
+    dissimilarity = skbio.stats.distance.DissimilarityMatrix(1-symmetric.toarray())
+    coordinates = skbio.stats.ordination.pcoa(dissimilarity)
+
+    # plotting principial coordinated
+    #plt.scatter(coordinates[0], coordinates[1], s=5)
+    #Total = sum(np.square(np.real(eigvals)))
+    #PC1 = round((np.square(np.real(eigvals[0]))/Total)*100, 2) 
+    #PC2 = round((np.square(np.real(eigvals[1]))/Total)*100, 2) 
+    #plt.xlabel(f"PC1 ({PC1}%)")
+    #plt.ylabel(f"PC2 ({PC2}%)")
+    #plt.title(f"PCoA of before optimization")
+    plt.scatter(coordinates.samples['PC1'], coordinates.samples['PC2'], s=5)
+    Total = sum(np.square(np.real(coordinates.eigvals)))
+    PC1 = round((np.square(np.real(coordinates.eigvals[0]))/Total)*100, 2) 
+    PC2 = round((np.square(np.real(coordinates.eigvals[1]))/Total)*100, 2) 
+    plt.xlabel(f"PC1 ({PC1}%)")
+    plt.ylabel(f"PC2 ({PC2}%)")
+    plt.title(f"PCoA of before optimization") 
+    #plt.savefig("test_1.png", format='png')
+    return plt.show()
+
+def contours(M, title):
+    x, y = np.gradient(M)
+    fig, ax = plt.subplots()
+    ax.contour(M, cmap="viridis")
+    ax.quiver(x, y)
+    ax.set_title(f"Contour plot of {title}")
+    fig.savefig(f"../{title}_contours.png", format='png')
+
+def gradient_plot_2D(M, title):
+    x, y = np.gradient(M)
+    fig, ax = plt.subplots()
+    im = ax.imshow(M, cmap="viridis")
+    ax.quiver(x, y)
+    ax.set_title(f"Gradient of {title}")
+    fig.savefig(f"../{title}_2D_GD.png", format='png')
+
+def gradient_plot_3D(M, title):
+    grad_x, grad_y = np.gradient(cscs_u)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    surf = ax.plot_surface(*np.meshgrid(np.arange(cscs_u.shape[0]), np.arange(cscs_u.shape[0])), cscs_u, cmap='viridis', linewidth=0)
+    # Controls the arrows
+    ax.quiver(*np.meshgrid(np.arange(cscs_u.shape[0]), np.arange(cscs_u.shape[0])), np.zeros_like(cscs_u), \
+        grad_x, grad_y, np.zeros_like(cscs_u),\
+            # Parameters for arrows
+            length=0.1, normalize=True, color='r')
+    # Set the labels and title
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_title(f"Gradient of {title}")
+    fig.savefig(f"../{title}_3D_GD.png", format='png')
+
+def pca(X):
+    mean = X.mean(axis=0) 
+    center = X - mean 
+    _, stds, pcs = np.linalg.svd(center/np.sqrt(X.shape[0])) 
+    return stds**2, pcs
+
+# Parallel C interface optimization
+os.environ["USE_INTEL_MKL"] = "1"
+mkl.set_num_threads(4)
+
+def data_dump(data, title):
+    file = open(f"../{title}", "wb")
+    pickle.dump(data, file)
+    file.close()
+
+def cscs(A, B, css):
+    AB = A * B.T * css
+    return np.sum(AB)
+
+def worker(input, output, css):
+    for func, A, B, index_a, index_b in iter(input.get, None):
+        result = func(A, B, css)
+        output.put([index_a, index_b, result])
+
+def Parallelize(func, samples, css):
+    NUMBER_OF_PROCESSES = mp.cpu_count()
+
+    cscs_w = np.zeros([samples.shape[1], samples.shape[1]])
+    TASKS = [(func, samples[:,i], samples[:,j], i, j) for i,j in itertools.combinations(range(0, samples.shape[1]), 2)]
+
+    # Create queues
+    task_queue = mp.Queue()
+    done_queue = mp.Queue()    
+
+    # Submit tasks
+    for task in TASKS:
+        task_queue.put(task)
+
+    # Start worker processes
+    for i in range(NUMBER_OF_PROCESSES):
+        mp.Process(target=worker, args=(task_queue, done_queue, css)).start()
+
+    # Get and print results
+    for i in range(len(TASKS)):
+        res = done_queue.get()
+        cscs_w[res[0],res[1]] = res[2]
+        cscs_w[res[1],res[0]] = cscs_w[res[0],res[1]]
+
+    # Tell child processes to stop
+    for i in range(NUMBER_OF_PROCESSES):
+        task_queue.put(None)
+
+    cscs_w[np.diag_indices(cscs_w.shape[0])] = 1 
+
+    return cscs_w 
+
+#---------------------------------------------------------------------------------------------------------------------#
+# simulated data
+#---------------------------------------------------------------------------------------------------------------------#
+
+# Set the dimensions of the data
+p = 3   # number of features
+n = 100  # number of samples# Set the mean and covariance of the data
+mean = np.zeros(p)
+cov = np.eye(p)   # covariance matrix# Generate data from the multivariate normal distribution
+data = np.random.multivariate_normal(mean, cov, n)# Add a binary covariate to the data
+x = np.random.binomial(n=1, p=0.5, size=n)  # binary covariate
+data[:, 0] += x  # add x to the first feature
+data.astype(np.float32)
+# Compute the similarity matrix
+css = np.cov(data)
+
+#---------------------------------------------------------------------------------------------------------------------#
+# Parallelization
+#---------------------------------------------------------------------------------------------------------------------#
+
+cscs_u = Parallelize(cscs, np.absolute(data), css)
+cscs_u.astype(np.float32)
+
+#---------------------------------------------------------------------------------------------------------------------#
+# Optimization algorithm
+#---------------------------------------------------------------------------------------------------------------------#
+
+def eig_loss(gradient):
+    eigval, eigvec = np.linalg.eig(gradient)
+    alpha = 2 / (eigval[0]+eigval[1])
+    loss = 0
+    k = np.max(eigval) / np.min(eigval)
+    error = np.arccos(np.dot(eigval[0],eigval[1])/(np.linalg.norm(eigval[0])*np.linalg.norm(eigval[1])))
+    loss = sum(error * ((1-(alpha*eigval[0]))**k) * eigvec[0] + error * ((1-(alpha*eigval[1]))**k) * eigvec[1])
+    return alpha, loss, eigval
+
+def GD_eigen(cscs_u):
+
+    #WW = np.zeros([cscs_u.shape[0], cscs_u.shape[0]])
+
+#for p, q in itertools.combinations(range(0, cscs_u.shape[1]), 2):
+    # two samples
+    #pair = np.outer(cscs_u[:,p], cscs_u[:,q])
+    #dA = cscs_u[:,p]
+    #dB = np.outer(cscs_u[:,q], cscs_u[:,q])
+
+    # approximate alpha and beta
+    sample_mean = np.mean(cscs_u)
+    sample_var = np.var(cscs_u, ddof=1)
+    alpha = sample_mean * (sample_mean * (1 - sample_mean) / sample_var - 1)
+    if alpha < 0:
+        alpha *= -1
+    beta = (1 - sample_mean) * (sample_mean * (1 - sample_mean) / sample_var - 1)
+    if beta < 0:
+        beta *= -1
+    #print(f"alpha: {alpha}\t beta: {beta}")
+    # weights sample with constraint
+    w = np.random.beta(alpha, beta, size=cscs_u.shape[0])
+    W = np.outer(w, w)
+    W.astype(np.float32)
+    #W = np.clip(W, 0, 1, dtype="float32")
+
+    # storing parameters
+    eigval1 = []
+    eigval2 = []
+    weights = []
+    alphas = []
+
+    # storing best weights based on alpha
+    best_alpha = 1.0
+    best_W = 0
+    best_iter = 0
+
+    # storing best weights based on eigenvalue 1
+    best_eig1 = 0
+    eig1_W = 0
+    eig1_it = 0
+    eig1_alpha = 0
+
+    for i in range(100):
+        # Eigen decomposition        
+            #Bu_eigval,_ = np.linalg.eig(dB)
+            #_, Aw_eigvec = np.linalg.eig(np.outer(dA, w))
+            ## Generalized symmetric pair of matrices
+            #grad = Aw_eigvec.T * (dA - (dB * np.outer(Bu_eigval, Bu_eigval))) * Aw_eigvec
+            M = cscs_u * W
+            _, eigvec_w = np.linalg.eig(M)
+            grad = eigvec_w * cscs_u * eigvec_w.T
+
+            # gradient error & alpha
+            alpha, loss, eigval = eig_loss(grad)
+            
+            # stores parameters
+            eigval1.append(np.real(eigval[0]))
+            eigval2.append(np.real(eigval[1]))
+            weights.append(np.sum(np.real(W)))
+            alphas.append(np.real(alpha))
+            
+            print(f"iter: {str(i)}\t alpha: {np.real(alpha)}\t eigval 1: {np.real(eigval[0])}\t eigval 2: {np.real(eigval[1])}\t loss: {loss}")
+            # this means PC1 and PC2 cover 99% of the cov.
+            if 0 <= alpha < best_alpha:
+                best_alpha = alpha
+                best_W = W
+                best_iter = i
+
+            if eigval[0] > best_eig1:
+                best_eig1 = eigval[0]
+                eig1_W = W
+                eig1_it = i
+                eig1_alpha = alpha
+
+            # Update weights
+            # parenthesis to allow complex to float casting
+            W = (W + alpha * grad)
+            #W = np.clip(W, 0, 1)
+
+    return eigval1, eigval2, weights, alphas, best_alpha, best_W, best_iter, eig1_W, eig1_it, eig1_alpha
+
+#---------------------------------------------------------------------------------------------------------------------#
+# Bare bones gradient descent
+#---------------------------------------------------------------------------------------------------------------------#
+def MSE(X_pred, X_true):
+    return np.sum((X_true - X_pred)**2) / len(X_true)
+
+def initialize_theta(X):
+    sample_mean = np.mean(X)
+    sample_var = np.var(X, ddof=1)
+    alpha = sample_mean * (sample_mean * (1 - sample_mean) / sample_var - 1)
+    if alpha < 0:
+        alpha *= -1
+    beta = (1 - sample_mean) * (sample_mean * (1 - sample_mean) / sample_var - 1)
+    if beta < 0:
+        beta *= -1
+    #print(f"alpha: {alpha}\t beta: {beta}")
+    # weights sample with constraint
+    w = np.random.beta(alpha, beta, size=X.shape[0])
+    W = np.outer(w, w)
+    W.astype(np.float32)
+    return W
+
+def grad(X, W):
+    M = X * W
+    _, eigvec_w = np.linalg.eig(M)
+    grad = eigvec_w * X * eigvec_w.T
+    return grad, M
+
+#def var_explained(gradient, X):
+#    eig_grad, _ = np.linalg.eig(gradient)
+#    eig_X, _ = np.linalg.eig(X)
+#    loss = np.sum(eig_X[:2])/np.sum(eig_X)
+#    alpha = 2 / (eig_grad[0] + eig_grad[1])
+#    return loss, alpha
+
+def Bare_bone(X, alpha, num_iters, epss = np.finfo(np.float32).eps):
+    W = initialize_theta(X)
+    df1 = pd.DataFrame(columns=["weight", "MSE"])
+    df2 = pd.DataFrame(columns=["weight", "eMSE", "eigval1", "eigval2"])
+
+    data_dump = []
+
+    # sum of first two eigenvalues vs total sum
+    eigval_u, _ = np.linalg.eig(X)
+    prev_var = np.sum(eigval_u[:2]) / np.sum(eigval_u)
+    for i in range(num_iters):
+        get_grad, X_pred = grad(X, W)
+        mse = MSE(X_pred, X)
+        
+        eigval, _ = np.linalg.eig(get_grad)
+        var_explained = np.sum(eigval[:2]/np.sum(eigval))
+        emse = prev_var - var_explained
+
+        # epss is based on the machine precision of float 32
+        if 0 < emse < epss:
+            break
+
+        #alpha, _ = eig_errors(get_grad)
+        #print(f"MSE: {prev_loss}\t eig_loss: {np.real(loss)}")
+        df1.loc[i] = [i, mse]
+        df2.loc[i] = [i, np.real(emse), np.real(eigval[0]), np.real(eigval[1])]
+        data_dump.append((i, W, X_pred))
+
+        W = W + (alpha * get_grad)
+        prev_var = var_explained
+    
+    return df1, df2
+
+def GD_alpha(X, num_iters):
+    W = initialize_theta(X)
+    df1 = pd.DataFrame(columns=["weight", "MSE"])
+    df2 = pd.DataFrame(columns=["weight", "eMSE"])
+
+    for i in range(num_iters):
+        get_grad, X_pred = grad(X, W)
+        mse = MSE(X_pred, X)
+        emse, alpha = eMSE(get_grad, X)
+        #alpha, _ = eig_errors(get_grad)
+        #print(f"MSE: {prev_loss}\t eig_loss: {np.real(loss)}")
+        df1.loc[i] = [np.sum(W), mse]
+        df2.loc[i] = [np.sum(W), np.real(emse)]
+        W = W - (alpha * get_grad)
+    
+    return df1, df2
+
+it = 20     
+df_mse1, df_emse1 = Bare_bone(cscs_u, alpha=0.001, num_iters=it)
+df_mse2, df_emse2 = Bare_bone(cscs_u, alpha=0.01, num_iters=it)
+df_mse3, df_emse3 = Bare_bone(cscs_u, alpha=0.1, num_iters=it)
+#df_mse4, df_emse4, _ = Bare_bone(css, alpha=1.1, num_iters=it)
+#df_mse5, df_emse5, _ = Bare_bone(css, alpha=1.5, num_iters=it)
+
+#df_mse, df_emse, _ = GD_alpha(cscs_u, it)
+
+#or i,j in zip(df_emse3["weight"], df_emse3["eMSE"]):
+#    print(f"iter: {i}\t var explained: {j}")
+"""
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(4)
+ax1.plot(df_mse1["weight"], df_mse1["MSE"], label="a=0.001")
+ax1.plot(df_mse2["weight"], df_mse2["MSE"], label="a=0.01")
+ax1.plot(df_mse3["weight"], df_mse3["MSE"], label="a=0.1")
+#ax1.plot(df_mse4["weight"], df_mse4["MSE"], label="a=1.1")
+#ax1.plot(df_mse5["weight"], df_mse5["MSE"], label="a=1.5")
+#ax1.plot(df_mse["weight"], df_mse["MSE"], label="a=2/e1+e2")
+ax1.set_xlabel(f"iterations")
+ax1.set_ylabel("MSE")
+ax2.plot(df_emse1["weight"], df_emse1["eMSE"], label="a=0.001")
+ax2.plot(df_emse2["weight"], df_emse2["eMSE"], label="a=0.01")
+ax2.plot(df_emse3["weight"], df_emse3["eMSE"], label="a=0.1")
+#ax2.plot(df_emse4["weight"], df_emse4["eMSE"], label="a=1.1")
+#ax2.plot(df_emse5["weight"], df_emse5["eMSE"], label="a=1.5")
+#ax2.plot(df_emse["weight"], df_emse["eMSE"], label="a=2/e1+e2")
+ax2.set_xlabel(f"iterations")
+ax2.set_ylabel("MSE of First two Eigenvalues ")
+ax3.plot(df_emse1["weight"], df_emse1["eigval1"], label="a=0.001")
+ax3.plot(df_emse2["weight"], df_emse2["eigval1"], label="a=0.01")
+ax3.plot(df_emse3["weight"], df_emse3["eigval1"], label="a=0.1")
+#ax2.plot(df_emse4["weight"], df_emse4["eMSE"], label="a=1.1")
+#ax2.plot(df_emse5["weight"], df_emse5["eMSE"], label="a=1.5")
+#ax2.plot(df_emse["weight"], df_emse["eMSE"], label="a=2/e1+e2")
+ax3.set_xlabel(f"iterations")
+ax3.set_ylabel("Eigenvalue 1")
+ax4.plot(df_emse1["weight"], df_emse1["eigval2"], label="a=0.001")
+ax4.plot(df_emse2["weight"], df_emse2["eigval2"], label="a=0.01")
+ax4.plot(df_emse3["weight"], df_emse3["eigval2"], label="a=0.1")
+#ax2.plot(df_emse4["weight"], df_emse4["eMSE"], label="a=1.1")
+#ax2.plot(df_emse5["weight"], df_emse5["eMSE"], label="a=1.5")
+#ax2.plot(df_emse["weight"], df_emse["eMSE"], label="a=2/e1+e2")
+ax4.set_xlabel(f"iterations")
+ax4.set_ylabel("Eigenvalue 2")
+ax1.legend()
+#plt.show()
+"""
+contours(cscs_u, title="CSCS_unweighted")
+gradient_plot_2D(cscs_u, title="CSCS_unweighted")
+gradient_plot_3D(cscs_u, title="CSCS_unweighted")
+
+
+#---------------------------------------------------------------------------------------------------------------------#
+# Visualizing simulated data
+#---------------------------------------------------------------------------------------------------------------------#
+"""
+pca_color = sns.color_palette(None, cscs_u.shape[1])
+eigval1, eigval2, weights, alphas, alpha, alpha_W, alpha_it, eig1_W, eig1_it, eig_alpha = GD_eigen(cscs_u)
+
+print(f"alpha: {alpha}\t eigenvalue alpha: {eig_alpha}")
+
+var_u, pcs_u = pca(cscs_u)
+var_w, pcs_w = pca(cscs_u*alpha_W)
+var_eig_w, pcs_eig_w = pca(cscs_u*eig1_W)
+### subplot 2 ###
+# font size
+font_size = 15
+plt.rcParams.update({"font.size": 12})
+
+fig0, (ax1, ax2) = plt.subplots(2)
+fig0.set_size_inches(15, 10)
+# unweigthed cscs
+for i in range(cscs_u.shape[1]):
+    ax1.scatter(pcs_u[0][i], pcs_u[1][i], color=pca_color[i], s=10, label=f"{i+1}")
+    ax1.annotate(f"{str(i+1)}", (pcs_u[0][i], pcs_u[1][i]))
+ax1.set_xlabel(f"PC1: {round(var_u[0]/np.sum(var_u)*100,2)}%")
+ax1.set_ylabel(f"PC2: {round(var_u[1]/np.sum(var_u)*100,2)}%")
+ax1.legend(loc='center left', bbox_to_anchor=(1, 0.7))
+
+# Weighted cscs
+for i in range(cscs_u.shape[1]):
+    ax2.scatter(pcs_eig_w[0][i], pcs_eig_w[1][i], color=pca_color[i], s=10, label=f"{i+1}")
+    ax2.annotate(f"{str(i+1)}", (pcs_eig_w[0][i], pcs_eig_w[1][i]))
+ax2.set_xlabel(f"PC1: {round(var_eig_w[0]/np.sum(var_eig_w)*100,2)}%")
+ax2.set_ylabel(f"PC2: {round(var_eig_w[1]/np.sum(var_eig_w)*100,2)}%")
+ax2.legend(loc='center left', bbox_to_anchor=(1, 0.7))
+fig0.savefig("../cscsw_PCA_eig.png", format='png')
+
+
+fig1, (ax1, ax2) = plt.subplots(2)
+fig1.set_size_inches(15, 10)
+# unweigthed cscs
+for i in range(cscs_u.shape[1]):
+    ax1.scatter(pcs_u[0][i], pcs_u[1][i], color=pca_color[i], s=10, label=f"{i+1}")
+    ax1.annotate(f"{str(i+1)}", (pcs_u[0][i], pcs_u[1][i]))
+ax1.set_xlabel(f"PC1: {round(var_u[0]/np.sum(var_u)*100,2)}%")
+ax1.set_ylabel(f"PC2: {round(var_u[1]/np.sum(var_u)*100,2)}%")
+ax1.legend(loc='center left', bbox_to_anchor=(1, 0.7))
+
+# Weighted cscs
+for i in range(cscs_u.shape[1]):
+    ax2.scatter(pcs_w[0][i], pcs_w[1][i], color=pca_color[i], s=10, label=f"{i+1}")
+    ax2.annotate(f"{str(i+1)}", (pcs_w[0][i], pcs_w[1][i]))
+ax2.set_xlabel(f"PC1: {round(var_w[0]/np.sum(var_w)*100,2)}%")
+ax2.set_ylabel(f"PC2: {round(var_w[1]/np.sum(var_w)*100,2)}%")
+ax2.legend(loc='center left', bbox_to_anchor=(1, 0.7))
+fig1.savefig("../cscsw_PCA_alpha.png", format='png')
+
+### subplot 2 ###
+fig2, (ax1, ax2) = plt.subplots(2)
+fig2.set_size_inches(15, 10)
+palette = sns.color_palette(None, 4)
+X = [i for i in range(len(eigval1))]
+# visualization
+ax1.scatter(X, eigval1, color=palette[0], s=5, label=f"eigenvalue 1")
+ax1.scatter(X, eigval2, color=palette[1], s=5, label=f"eigenvalue 2")
+ax1.axvline(x=alpha_it, ls='--') # point of best alpha
+ax1.axvline(x=eig1_it, ls='-')
+ax1.set_ylabel(f"Eigenvalues")
+ax1.legend(loc='center left', bbox_to_anchor=(1, 0.7))
+
+ax2.scatter(X, weights, color=palette[2], s=5, label=f"weights")
+ax2.scatter(X, alphas, color=palette[3], s=5, label=f"alpha")
+ax2.axvline(x=alpha_it, ls='--')
+ax2.axvline(x=eig1_it, ls='-')
+ax2.set_xlabel(f"iterations")
+ax2.set_ylabel(f"weight & alpha")
+ax2.legend(loc='upper left', bbox_to_anchor=(1, 0.8))
+fig2.savefig("../cscsw_parameters.png", format='png')
+
+"""
